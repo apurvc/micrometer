13faa8fd2714780c0231cc3ea372f81b405bc63a
Apurv Chandra <apurvc@gmail.com>
2023-11-14 20:48:38 +0000

    updated code.

--- src/main/java/com/cmpg/demo/MyMeterRegistryConfiguration.java
+++ src/main/java/com/cmpg/demo/MyMeterRegistryConfiguration.java
@@ -3,13 +3,13 @@ package com.cmpg.demo;
 import org.springframework.context.annotation.Configuration;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 
 @Configuration(proxyBeanMethods = false)
 public class MyMeterRegistryConfiguration {
 
     @Bean
     public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-    return (registry) -> registry.config().commonTags("Type", "k8s_pod");
+    return (registry) -> registry.config().commonTags("cluster_name","cluster-1","pod_name","micrometer","namespace_name","default","projectId","playground-s-11-2af6ea26");
 }
-}
+}   
